7f40c02b6b319aaf18b75c5c9290dad054b57888
beatrice <beatrice.leong98@gmail.com>
2021-07-06 14:24:35 +0800

    portal.py and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,30 @@
 #! /bin/sh
 
-cd ./src/engine
-echo "Installing Python Environment for Portal..."
-python3 -m pip install --upgrade pip
-python3 -m pip install pipenv
-PIPENV_VENV_IN_PROJECT=1 PIPENV_DEFAULT_PYTHON_VERSION=3.7 pipenv sync -d
-cd ../app
+echo "Running setup bash job!"
 echo "Installing Nextjs Environment for Portal..."
 npm install
+cd ./src/app
+npm install
+
+echo "Building and Exporting related app files to portal-build..."
+npm run build:static
+
 cd ../..
+
+echo "Copying related app files to portal-build..."
+cp ./src/app/main.js ./portal-build
+cp ./src/app/preload.js ./portal-build
+
+echo "Copying related engine files to portal-build..."
+cp -R ./src/engine/server ./portal-build
+cp ./src/engine/run.py ./portal-build
+
+cd ./src/engine
+echo "Installing Python Environment in portal-build..."
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+cd ../..
+
 echo "Ending setup bash job in 10 secs..."
 sleep 10
