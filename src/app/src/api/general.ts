ba11e127b395cd129092bbc76b8595b24c5e712d
beatrice <beatrice.leong98@gmail.com>
2021-07-09 17:51:08 +0800

    Add new api call

--- src/app/src/api/general.ts
+++ src/app/src/api/general.ts
@@ -1,8 +1,20 @@
 import axios, { AxiosResponse } from "axios";
-import { SERVER_ADDRESS, CACHE, IS_ALIVE } from "@portal/constants/api";
+import {
+  SERVER_ADDRESS,
+  CACHE,
+  IS_ALIVE,
+  SHUTDOWN,
+  GET_GPU,
+  SET_GPU,
+  CLEAR_GPU,
+} from "@portal/constants/api";
 
 export function APIIsAlive(): Promise<AxiosResponse<any>> {
   return axios.get(SERVER_ADDRESS + IS_ALIVE);
+}
+
+export function APIShutdown(): Promise<AxiosResponse<any>> {
+  return axios.get(SERVER_ADDRESS + SHUTDOWN);
 }
 
 export function APILoadCache(): Promise<AxiosResponse<any>> {
@@ -11,4 +23,18 @@ export function APILoadCache(): Promise<AxiosResponse<any>> {
 
 export function APIRejectCache(): Promise<AxiosResponse<any>> {
   return axios.put(SERVER_ADDRESS + CACHE);
+}
+
+/* -------------- GPU API -------------- */
+
+export function APIGetGPU(): Promise<AxiosResponse<any>> {
+  return axios.get(SERVER_ADDRESS + GET_GPU);
+}
+
+export function APISetGPU(): Promise<AxiosResponse<any>> {
+  return axios.post(SERVER_ADDRESS + SET_GPU);
+}
+
+export function APIClearGPU(): Promise<AxiosResponse<any>> {
+  return axios.post(SERVER_ADDRESS + CLEAR_GPU);
 }
