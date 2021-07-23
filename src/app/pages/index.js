5f0adfc698f870c8dfba282389971125a7c4739c
marcus neo <56596363+marcus-neo@users.noreply.github.com>
2021-07-23 19:50:50 +0800

    Updated favicon to support light mode and dark mode

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -67,7 +67,8 @@ class Annotation extends React.Component {
       <>
         <Head>
           <title>Portal</title>
-          <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="./portal-ico-dark.png" media="(prefers-color-scheme:dark)" />
+          <link rel="icon" type="image/x-icon" href="./portal-ico-light.png" media="(prefers-color-scheme:light)" />
           <link
             rel="stylesheet"
             href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
--- src/app/pages/loading.js
+++ src/app/pages/loading.js
@@ -33,7 +33,8 @@ class Loading extends React.Component {
       <>
         <Head>
           <title>Loading</title>
-          <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="./portal-ico-dark.png" media="(prefers-color-scheme:dark)" />
+          <link rel="icon" type="image/x-icon" href="./portal-ico-light.png" media="(prefers-color-scheme:light)" />
           <link href="./static/style/annotation.css" rel="stylesheet" />
           <link
             href={
