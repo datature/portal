bfcfe365a13653e53adee7cdb08fd082c12f9550
SolarRabbit <solarrabbitofficial@gmail.com>
2023-04-13 14:32:49 +0800

    Improve code quality for analytics bar component

--- src/app/src/components/annotations/analyticsbar.tsx
+++ src/app/src/components/annotations/analyticsbar.tsx
@@ -1,31 +1,26 @@
-import React, { ReactElement } from "react";
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import React from "react";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
 type AnalyticsBarProps = {
-    analyticsResults: Array<Array<any>>
+    analyticsResults: Record<number, Array<any>>
     confidence: number
 }
 
 export function AnalyticsBar(props: AnalyticsBarProps) {
-    const [data, setData] = React.useState<any[]>([])
-    const [lines, setLines] = React.useState<ReactElement[]>([])
-    React.useEffect(()=>{
-        const newData:any[] = []
-        const newLines = new Set()
-        for(var k in props.analyticsResults) {
-            const dataPoint: any = { "frame": k }
-            props.analyticsResults[k].forEach(v => {
-                if(v.confidence < props.confidence) return
-                dataPoint[v.tag.name] = (dataPoint[v.tag.name] || 0) + 1
-                newLines.add(v.tag.name)
-            })
-            newData.push(dataPoint)
-        }
-        console.log(newData)
-        console.log(newLines)
-        setData(newData)
-        setLines(Array.from(newLines).map(str => <Line key={str} type='monotone' dataKey={str} />))
-    }, [props.analyticsResults, props.confidence])
+    const data = Object.entries(props.analyticsResults).map(e => {
+        const [k, v] = e
+        const dataPoint: any = { "frame": k }
+        v.filter(e => e.confidence >= props.confidence).forEach(e => {
+            dataPoint[e.tag.name] = (dataPoint[e.tag.name] || 0) + 1
+        })
+        return dataPoint
+    })
+
+    const lines : any[] = Array.from(new Set(Object.entries(props.analyticsResults)
+            .flatMap(e => e[1].filter(e => e.confidence >= props.confidence).map(e => e.tag.name))))
+            .map(str => <Line key={str} type='monotone' dataKey={str} />)
+
+    // Width and height are temporary
     return (<LineChart width={1400} height={120} data={data}>
                 <XAxis dataKey='frame' />
                 <YAxis />
